f13b3c49c6ef06d600e4d27088cb5a81b637f08a
Rami <r.sarieddine@live.com>
2015-03-12 11:56:32 +0400

    initial commit

---
+++ CoDiWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CoDiWeb.Startup))]
+namespace CoDiWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
